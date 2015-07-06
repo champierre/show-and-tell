ccb463442bb6adb40ffca5f90c0b1dd866a79c9b
Junya Ishihara <junya@champierre.com>
2015-07-07 01:09:07 +0900

    show scratch project

--- app/models/project.rb
+++ app/models/project.rb
@@ -5,4 +5,10 @@ class Project < ActiveRecord::Base
     movie_id = $1
     "<iframe width=\"640\" height=\"360\" src=\"https://www.youtube.com/embed/#{movie_id}?rel=0\" frameborder=\"0\" allowfullscreen></iframe>".html_safe
   end
+
+  def scratch_embed_url
+    scratch_url =~ /scratch\.mit\.edu\/projects\/(\d+)\//
+    scratch_project_id = $1
+    "<iframe allowtransparency=\"true\" width=\"485\" height=\"402\" src=\"//scratch.mit.edu/projects/embed/#{scratch_project_id}/?autostart=false\" frameborder=\"0\" allowfullscreen></iframe>".html_safe
+  end
 end
